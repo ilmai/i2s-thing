0ddc4d446ee9f4aa7a3e3ab4915b698dfd5ee5e8
Jussi Viiri <ilmai@iki.fi>
2022-09-19 23:43:52 +0300

    Add basic file operation functions

--- i2s-thing.c
+++ i2s-thing.c
@@ -1,3 +1,5 @@
+#include <evl/file.h>
+#include <evl/flag.h>
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
@@ -8,9 +10,12 @@
 #define DEVICE_CLASS_NAME "i2s-thing-class"
 #define DRIVER_NAME "i2s-thing-io"
 
-static int majorNumber;
-static struct class *charDeviceClass = NULL;
-static struct device *charDevice = NULL;
+static int major_number;
+static struct class *device_class = NULL;
+static struct device *char_device = NULL;
+static struct evl_file efile;
+static struct evl_flag eflag;
+static bool char_device_opened = false;
 
 struct i2s_thing_device
 {
@@ -19,9 +24,20 @@ struct i2s_thing_device
 
 static int i2s_thing_probe(struct platform_device *pdev);
 
-static struct file_operations fops =
-{
+static int i2s_thing_open(struct inode *inode, struct file *file);
+static int i2s_thing_release(struct inode *inode, struct file *file);
+static ssize_t i2s_thing_read(struct file *file, char __user *buf, size_t size);
+static ssize_t i2s_thing_write(struct file *file, const char __user *buf, size_t size);
+static long i2s_thing_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
+static struct file_operations i2s_thing_fops =
+{
+	.owner = THIS_MODULE,
+	.open = i2s_thing_open,
+	.release = i2s_thing_release,
+	.oob_read = i2s_thing_read,
+	.oob_write = i2s_thing_write,
+	.oob_ioctl = i2s_thing_ioctl,
 };
 
 static const struct of_device_id i2s_thing_of_match[] = {
@@ -44,28 +60,28 @@ static int __init i2s_thing_init(void)
 {
 	int ret;
 
-	majorNumber = register_chrdev(0, CHAR_DEVICE_NAME, &fops);
-	if (majorNumber < 0)
+	major_number = register_chrdev(0, CHAR_DEVICE_NAME, &i2s_thing_fops);
+	if (major_number < 0)
 	{
 		printk(KERN_ALERT "Failed to register major number\n");
-		return majorNumber;
+		return major_number;
 	}
 
-	charDeviceClass = class_create(THIS_MODULE, DEVICE_CLASS_NAME);
-	if (IS_ERR(charDeviceClass))
+	device_class = class_create(THIS_MODULE, DEVICE_CLASS_NAME);
+	if (IS_ERR(device_class))
 	{
-		unregister_chrdev(majorNumber, CHAR_DEVICE_NAME);
+		unregister_chrdev(major_number, CHAR_DEVICE_NAME);
 		printk(KERN_ALERT "Failed to create device class\n");
-		return PTR_ERR(charDeviceClass);
+		return PTR_ERR(device_class);
 	}
 
-	charDevice = device_create(charDeviceClass, NULL, MKDEV(majorNumber, 0), NULL, CHAR_DEVICE_NAME);
-	if (IS_ERR(charDevice))
+	char_device = device_create(device_class, NULL, MKDEV(major_number, 0), NULL, CHAR_DEVICE_NAME);
+	if (IS_ERR(char_device))
 	{
-		class_destroy(charDeviceClass);
-		unregister_chrdev(majorNumber, CHAR_DEVICE_NAME);
+		class_destroy(device_class);
+		unregister_chrdev(major_number, CHAR_DEVICE_NAME);
 		printk(KERN_ALERT "Failed to create device\n");
-		return PTR_ERR(charDevice);
+		return PTR_ERR(char_device);
 	}
 
 	i2s_thing_driver.driver.owner = THIS_MODULE;
@@ -74,10 +90,10 @@ static int __init i2s_thing_init(void)
 	ret = driver_register(&i2s_thing_driver.driver);
 	if (ret)
 	{
-		device_destroy(charDeviceClass, MKDEV(majorNumber, 0));
-		class_unregister(charDeviceClass);
-		class_destroy(charDeviceClass);
-		unregister_chrdev(majorNumber, CHAR_DEVICE_NAME);
+		device_destroy(device_class, MKDEV(major_number, 0));
+		class_unregister(device_class);
+		class_destroy(device_class);
+		unregister_chrdev(major_number, CHAR_DEVICE_NAME);
 
 		printk(KERN_ALERT "Failed to register driver\n");
 		return ret;
@@ -89,12 +105,58 @@ static int __init i2s_thing_init(void)
 static void __exit i2s_thing_exit(void)
 {
 	driver_unregister(&i2s_thing_driver.driver);
-	device_destroy(charDeviceClass, MKDEV(majorNumber, 0));
-	unregister_chrdev(majorNumber, CHAR_DEVICE_NAME);
-	class_destroy(charDeviceClass);
+	device_destroy(device_class, MKDEV(major_number, 0));
+	unregister_chrdev(major_number, CHAR_DEVICE_NAME);
+	class_destroy(device_class);
 }
 
 static int i2s_thing_probe(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static int i2s_thing_open(struct inode *inode, struct file *file)
+{
+	int ret;
+
+	// Only allow one access at a time
+	if (char_device_opened)
+	{
+		return -EBUSY;
+	}
+
+	ret = evl_open_file(&efile, file);
+	if (ret)
+	{
+		return ret;
+	}
+
+	evl_init_flag(&eflag);
+
+	char_device_opened = true;
+	return 0;
+}
+
+static int i2s_thing_release(struct inode *inode, struct file *file)
+{
+	evl_destroy_flag(&eflag);
+	evl_release_file(&efile);
+
+	char_device_opened = false;
+	return 0;
+}
+
+static ssize_t i2s_thing_read(struct file *file, char __user *buf, size_t size)
+{
+	return 0;
+}
+
+static ssize_t i2s_thing_write(struct file *file, const char __user *buf, size_t size)
+{
+	return 0;
+}
+
+static long i2s_thing_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	return 0;
 }
