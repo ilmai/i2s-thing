ad9fec0c110c94ea19c327012c79a6c7d72eb58a
Jussi Viiri <ilmai@iki.fi>
2022-09-20 09:23:34 +0300

    Register mapping, DMA setup

--- i2s-thing.c
+++ i2s-thing.c
@@ -1,14 +1,30 @@
 #include <evl/file.h>
 #include <evl/flag.h>
+#include <linux/dmaengine.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/of_address.h>
+#include <linux/regmap.h>
 #include <linux/platform_device.h>
 
 #define CHAR_DEVICE_NAME "i2s-thing"
 #define DEVICE_CLASS_NAME "i2s-thing-class"
 #define DRIVER_NAME "i2s-thing-io"
+
+// Registers
+#define CS_A		0x00
+#define FIFO_A		0x04
+#define MODE_A		0x08
+#define RXC_A		0x0c
+#define TXC_A		0x10
+#define DREQ_A		0x14
+#define INTEN_A		0x18
+#define INTSTC_A	0x1c
+#define GRAY		0x20
+
+// Register fields
 
 static int major_number;
 static struct class *device_class = NULL;
@@ -19,10 +35,13 @@ static bool char_device_opened = false;
 
 struct i2s_thing_device
 {
-
+	struct dma_chan *dma_chan_tx;
+	struct dma_chan *dma_chan_rx;
+	struct regmap	*regmap;
 };
 
 static int i2s_thing_probe(struct platform_device *pdev);
+static int i2s_thing_remove(struct platform_device *pdev);
 
 static int i2s_thing_open(struct inode *inode, struct file *file);
 static int i2s_thing_release(struct inode *inode, struct file *file);
@@ -50,16 +69,32 @@ static const struct of_device_id i2s_thing_of_match[] = {
 
 static struct platform_driver i2s_thing_driver = {
 	.probe = i2s_thing_probe,
+	.remove = i2s_thing_remove,
 	.driver = {
 		.name = DRIVER_NAME,
 		.of_match_table = i2s_thing_of_match,
 	},
 };
 
+static const struct regmap_config i2s_thing_regmap_config = {
+	.reg_bits = 32,
+	.reg_stride = 4,
+	.val_bits = 32,
+	.max_register = GRAY,
+	.cache_type = REGCACHE_NONE,
+};
+
+struct dma_slave_config i2s_thing_dma_config =
+{
+	.dst_addr_width = sizeof(u32),
+	.src_addr_width = sizeof(u32),
+};
+
 static int __init i2s_thing_init(void)
 {
 	int ret;
 
+	// Create character device
 	major_number = register_chrdev(0, CHAR_DEVICE_NAME, &i2s_thing_fops);
 	if (major_number < 0)
 	{
@@ -84,6 +119,7 @@ static int __init i2s_thing_init(void)
 		return PTR_ERR(char_device);
 	}
 
+	// Create I2S driver
 	i2s_thing_driver.driver.owner = THIS_MODULE;
 	i2s_thing_driver.driver.bus = &platform_bus_type;
 
@@ -110,8 +146,78 @@ static void __exit i2s_thing_exit(void)
 	class_destroy(device_class);
 }
 
+static int config_dma_channel(struct device* dev, struct dma_chan** dma_chan, const char* name)
+{
+	*dma_chan = dma_request_chan(dev, name);
+
+	if (IS_ERR(*dma_chan))
+	{
+		dev_err(dev, "DMA TX channel request failed\n");
+		*dma_chan = NULL;
+		return PTR_ERR(*dma_chan);
+	}
+
+	dmaengine_slave_config(*dma_chan, &i2s_thing_dma_config);
+
+	return 0;
+}
+
 static int i2s_thing_probe(struct platform_device *pdev)
 {
+	struct i2s_thing_device *i2s_thing_dev;
+	void __iomem *register_address;
+	const __be32 *dma_address_be32;
+	dma_addr_t dma_address;
+
+	// Custom device data
+	i2s_thing_dev = devm_kzalloc(&pdev->dev, sizeof(*i2s_thing_dev), GFP_KERNEL);
+	if (!i2s_thing_dev)
+	{
+		return -ENOMEM;
+	}
+
+	dev_set_drvdata(&pdev->dev, i2s_thing_dev);
+
+	// Register map
+	register_address = devm_platform_ioremap_resource(pdev, 0);
+	if (IS_ERR(register_address))
+	{
+		return PTR_ERR(register_address);
+	}
+
+	i2s_thing_dev->regmap = devm_regmap_init_mmio(&pdev->dev, register_address, &i2s_thing_regmap_config);
+	if (IS_ERR(i2s_thing_dev->regmap))
+	{
+		return PTR_ERR(i2s_thing_dev->regmap);
+	}
+
+	// DMA
+	dma_address_be32 = of_get_address(pdev->dev.of_node, 0, NULL, NULL);
+	if (!dma_address_be32)
+	{
+		dev_err(&pdev->dev, "Error getting DMA address\n");
+		return -EINVAL;
+	}
+	dma_address = be32_to_cpup(dma_address_be32);	
+
+	i2s_thing_dma_config.src_addr = dma_address + FIFO_A;
+	i2s_thing_dma_config.dst_addr = dma_address + FIFO_A;
+
+	config_dma_channel(&pdev->dev, &i2s_thing_dev->dma_chan_tx, "tx");
+	config_dma_channel(&pdev->dev, &i2s_thing_dev->dma_chan_rx, "rx");
+
+	return 0;
+}
+
+static int i2s_thing_remove(struct platform_device *pdev)
+{
+	struct i2s_thing_device *i2s_thing_dev;
+
+	i2s_thing_dev = dev_get_drvdata(&pdev->dev);
+	
+	dma_release_channel(i2s_thing_dev->dma_chan_tx);
+	dma_release_channel(i2s_thing_dev->dma_chan_rx);
+
 	return 0;
 }
 
